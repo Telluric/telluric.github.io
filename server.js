1f73d207ab7b9d0576aca63b5c4790acbe5a148f
PhearZero <github@phearzero.com>
2016-02-04 16:22:31 -0600

    Updating url and port for Heroku

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var ghost = require('ghost');
+
+
 ghost().then(function (ghostServer) {
+    ghostServer.config.url = 'https://telluric-github-io.herokuapp.com';
+    ghostServer.config.server.port = process.env.PORT || 8080;
     ghostServer.start();
 });
