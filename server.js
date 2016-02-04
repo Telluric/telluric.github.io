c4443303a777969a1f2194111c15712fd51b1520
PhearZero <github@phearzero.com>
2016-02-04 16:35:31 -0600

    Adding mysql block and setting SSL

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var ghost = require('ghost');
-
-
 ghost().then(function (ghostServer) {
-    ghostServer.config.url = 'https://telluric-github-io.herokuapp.com';
+    //var db = {
+    //    client: 'mysql',
+    //        connection: {
+    //        host     : '127.0.0.1',
+    //            user     : 'your_database_user',
+    //            password : 'your_database_password',
+    //            database : 'ghost_db',
+    //            charset  : 'utf8'
+    //    }
+    //}
+
+    ghostServer.config.url = 'http://telluric-github-io.herokuapp.com';
+    ghostServer.config.urlSSL = 'https://telluric-github-io.herokuapp.com';
+
     ghostServer.config.server.port = process.env.PORT || 8080;
     ghostServer.start();
 });
